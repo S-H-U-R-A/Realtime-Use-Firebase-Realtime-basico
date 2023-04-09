3e88415387d20409420bb5f11a0c2bb4eb8b8cd5
sandres0190 <sandres0190@gmail.com>
2023-04-09 16:22:33 -0500

    SE APRENDE COMO OBTENER UNA BD, SUS NODOS, MODIFICAR EL VALOR DEL NODO, ELIMINARLOS ESTAS ESCUCHANDO CUANDO HACEMOS ESTAS ACCIONES, ADEMAS DE ESCUCHAR EL NODO EN SI MISMO POR SI TUVO ALGUN CAMBIO, ADEMÁS SE HABILITO LA PERSISTENCIA EN DISCO

--- app/src/main/java/com/example/holafirebase/MainActivity.kt
+++ app/src/main/java/com/example/holafirebase/MainActivity.kt
@@ -2,7 +2,13 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.holafirebase.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
  class MainActivity : AppCompatActivity() {
 
@@ -14,9 +20,55 @@ import com.example.holafirebase.databinding.ActivityMainBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //ACEDEMOS A LA RAIZ DE LA BD EN REMOTO
+        val dataBase = Firebase.database.reference
+        //OBTENEMOS EL NODO/PROPIEDAD QUE QUEREMOS OBSERVAR
+        val dataRef = dataBase.child("hola_firebase").child("data")
 
+        //SE CREA EL LISTENER DE LA PROPIEDAD QUE QUEREMOS OBSERVAR
+        val listener = object : ValueEventListener{
+            //ESTE MÉTODO ES EL OYENTE DE LA BD EN REMOTO
+            override fun onDataChange(snapshot: DataSnapshot) {
 
-        binding.tvData.text = "FireBase Remote: "
+                //SI LA PROPIEDAD EXISTE
+                if(snapshot.exists()){
+                    //SE OBTIENE EL VALOR EN EL MOMENTO DE LA PROPIEDAD EN REMOTO
+                    //A LA CUAL SE LE ASIGNE ESTE LISTENER
+                    val data = snapshot.getValue( String::class.java )
+
+                    binding.tvData.text = "FireBase Remote: $data"
+                }else{
+                    binding.tvData.text = "Ruta sin datos"
+                }
+
+            }
+
+            //SI OCURRE UN ERROR AL ESCUCHAR
+            override fun onCancelled(error: DatabaseError) {
+
+                Toast.makeText(this@MainActivity, "Error al leer datos", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+        //ASIGNAMOS A EL NODO EL LISTENER
+        dataRef.addValueEventListener(listener)
+
+        //COMO YA TENEMOS LA REFERENCIA DE NODO EN FIREBASE,
+        //PODEMOS EDITAR LA VARIABLE DESDE AQUI
+        binding.btnSend.setOnClickListener {
+            val data = binding.etData.text.toString()
+            dataRef.setValue(data)
+                .addOnSuccessListener { Toast.makeText(this@MainActivity, "Dato Enviado", Toast.LENGTH_SHORT).show()  }
+                .addOnFailureListener { Toast.makeText(this@MainActivity, "Error al Enviar", Toast.LENGTH_SHORT).show() }
+                .addOnCompleteListener { Toast.makeText(this@MainActivity, "Acción Completada", Toast.LENGTH_SHORT).show() }
+        }
+
+        //COMO ELIMINAR UNA REFERENCIA
+        binding.btnSend.setOnLongClickListener{
+            dataRef.removeValue()
+            true
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/holafirebase/MyApp.kt
@@ -0,0 +1,16 @@
+package com.example.holafirebase
+
+import android.app.Application
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Firebase.database.setPersistenceEnabled(true)
+
+    }
+
+}
